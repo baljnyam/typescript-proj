84c68b909bf376f5ac34028c43ea86361bdcf45f
baljnyam <baljnyam@gmail.com>
2020-01-27 22:46:56 +1100

    function, class, interface

---
+++ greeter.ts
@@ -0,0 +1,50 @@
+// // function
+// function greeter(person) {
+//   return "Hello, " + person;
+// }
+
+// let user = "Jane User";
+
+// document.body.textContent = greeter(user);
+
+// interface
+
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+
+// function greeter(person: Person) {
+//   return "Hello, " + person.firstName + " " + person.lastName;
+// }
+
+// let user = { firstName: "John", lastName: "User" };
+
+// document.body.textContent = greeter(user);
+// console.log(greeter(user));
+
+// Class
+
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
